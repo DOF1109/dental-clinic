ad2685699e3d53c75afcb2d3b82ebd2f0c37e1fa
DOF1109 <friasdaniel.1109@gmail.com>
2023-11-12 18:27:28 -0300

    Trabajando sobre modificar odontologo Se agrega preventDefault()

--- src/main/resources/static/js/put_odontologo.js
+++ src/main/resources/static/js/put_odontologo.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function () {
-
-
     //Buscamos y obtenemos el formulario donde estan
     //los datos que el usuario pudo haber modificado de la odontologo
     const formulario = document.querySelector('#update_odontologo_form');
 
     formulario.addEventListener('submit', function (event) {
+        event.preventDefault();
+
         let odontologoId = document.querySelector('#odontologo_id').value;
 
         //creamos un JSON que tendrá los datos del odontologo
@@ -28,35 +28,40 @@ window.addEventListener('load', function () {
             },
             body: JSON.stringify(formData)
         }
+
         fetch(url,settings)
-        .then(response => response.json())
+        .then(response => response.json()) // return the result of parsing the JSON
         .then(data => {
+            console.log("Recargué");
+            // do something with the data if needed
             location.reload();
-        }).catch(error => {
-            alert("Error: " + error);
+        })
+        .catch(error => {
+            console.log("Error: " + error);
         })
     })
- })
 
-    //Es la funcion que se invoca cuando se hace click sobre el id de una odontologo del listado
-    //se encarga de llenar el formulario con los datos de la odontologo
-    //que se desea modificar
-    function findBy(id) {
-          const url = '/odontologo'+"/"+id;
-          const settings = {
-              method: 'GET'
-          }
-          fetch(url,settings)
-          .then(response => response.json())
-          .then(data => {
-              let odontologo = data;
-              document.querySelector('#odontologo_id').value = odontologo.id;
-              document.querySelector('#matricula').value = odontologo.matricula;
-              document.querySelector('#nombre').value = odontologo.nombre;
-              document.querySelector('#apellido').value = odontologo.apellido;
-              //el formulario por default esta oculto y al editar se habilita
-              document.querySelector('#div_odontologo_updating').style.display = "block";
-          }).catch(error => {
-              alert("Error: " + error);
-          })
-      }
+})
+
+//Es la funcion que se invoca cuando se hace click sobre el id de una odontologo del listado
+//se encarga de llenar el formulario con los datos de la odontologo
+//que se desea modificar
+function findBy(id) {
+    const url = '/odontologo'+"/"+id;
+    const settings = {
+        method: 'GET'
+    }
+    fetch(url,settings)
+    .then(response => response.json())
+    .then(data => {
+        let odontologo = data;
+        document.querySelector('#odontologo_id').value = odontologo.id;
+        document.querySelector('#matricula').value = odontologo.matricula;
+        document.querySelector('#nombre').value = odontologo.nombre;
+        document.querySelector('#apellido').value = odontologo.apellido;
+        //el formulario por default esta oculto y al editar se habilita
+        document.querySelector('#div_odontologo_updating').style.display = "block";
+    }).catch(error => {
+        alert("Error: " + error);
+    })
+}
