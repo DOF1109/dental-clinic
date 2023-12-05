0e9567b33a31426fe9883cf41d3b880b75540c30
arielibarbalz <ibarbalz@gmail.com>
2023-12-04 23:58:43 -0300

    SE actuliza DTO

--- src/main/java/com/odontologia/ClinicaOdontologica/dto/TurnoDTO.java
+++ src/main/java/com/odontologia/ClinicaOdontologica/dto/TurnoDTO.java
@@ -11,16 +11,28 @@ import java.time.LocalDate;
 public class TurnoDTO {
     private Long id;
     private Long pacienteId;
+    private String pacienteNombre;
+    private String pacienteApellido;
+    private String cedula;
     private Long odontologoId;
+    private String matricula;
+    private String odontologoNombre;
+    private String odontologoApellido;
     private LocalDate fechaTurno;
-
     public TurnoDTO() {
     }
-
-    public TurnoDTO(Long id, Long pacienteId, Long odontologoId, LocalDate fechaTurno) {
+    public TurnoDTO(Long id, Long pacienteId, String pacienteNombre, String pacienteApellido, String cedula, Long odontologoId, String matricula, String odontologoNombre, String odontologoApellido, LocalDate fechaTurno) {
         this.id = id;
         this.pacienteId = pacienteId;
+        this.pacienteNombre = pacienteNombre;
+        this.pacienteApellido = pacienteApellido;
+        this.cedula = cedula;
         this.odontologoId = odontologoId;
+        this.matricula = matricula;
+        this.odontologoNombre = odontologoNombre;
+        this.odontologoApellido = odontologoApellido;
         this.fechaTurno = fechaTurno;
     }
 }
+
+
